739106124078a3c85192324436f6ad1ee1ddbc6e
JackyLin <605960704@qq.com>
2020-05-17 19:09:17 +0100

    add obstacles_avoidance

--- controllers/our_controller/our_controller.py
+++ controllers/our_controller/our_controller.py
@@ -24,21 +24,64 @@ for i in range(2):
     wheels[i].setPosition(float('inf'))
     wheels[i].setVelocity(0.0)
     
-avoidObstacleCounter = 0
+f_ObstacleCounter = 0
+fl_ObstacleCounter = 0
+fr_ObstacleCounter = 0
+turn_counter = 0
+turnR = True
+move_counter = 0
+adjust = 0
 
 while robot.step(TIME_STEP) != -1:
     
-    leftSpeed = 3.0
-    rightSpeed = 3.0
+    leftSpeed = 7
+    rightSpeed = 7
     
-    if avoidObstacleCounter > 0:
-        avoidObstacleCounter -= 1
-        leftSpeed = 3.0
-        rightSpeed = -3.0
+    if adjust > 0:
+        adjust -= 1
+        leftSpeed = -3.4
+        rightSpeed = 3.4
+    
+    elif f_ObstacleCounter > 0 and turnR:
+        f_ObstacleCounter -= 1
+        leftSpeed = 5.0
+        rightSpeed = -5.0
+        turn_counter += 1
+        if turn_counter == 10:
+           turnR = False
+           turn_counter = 0
+           
+    elif f_ObstacleCounter > 0 and not turnR:
+        f_ObstacleCounter -= 1
+        leftSpeed = -5.0
+        rightSpeed = 5.0
+        turn_counter += 1
+        if turn_counter == 10:
+           turnR = True
+           turn_counter = 0    
+
+    elif fl_ObstacleCounter > 0:
+        fl_ObstacleCounter -= 1
+        leftSpeed = 5.0
+        rightSpeed = -5.0
+        
+    elif fr_ObstacleCounter > 0:
+        fr_ObstacleCounter -= 1
+        leftSpeed = -5.0
+        rightSpeed = 5.0
+        
     else:  # read sensors
-        for i in range(2):
-            if ds[i].getValue() < 950.0:
-                avoidObstacleCounter = 100
+        if ds[0].getValue() < 950.0:
+            f_ObstacleCounter = 7
+        elif ds[1].getValue() < 950.0:
+            fl_ObstacleCounter = 7 
+        elif ds[3].getValue() < 950.0:
+            fr_ObstacleCounter = 7  
+        else:
+            move_counter += 1
+            if move_counter == 50:
+                adjust = 4
+                move_counter = 0     
                 
     wheels[0].setVelocity(leftSpeed)
     wheels[1].setVelocity(rightSpeed)
